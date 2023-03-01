ebb405b4213df2090ae0ca78aec1fe705c0324ee
comenttGT <100043986+comenttGT@users.noreply.github.com>
2023-03-01 15:15:26 +0100

    Source Code

---
+++ OldMirror/OldMirror/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using BepInEx;
+
+namespace OldMirror
+{
+	[BepInPlugin("comenttGT", "OldMirror", "1.0.0")]
+	public class Plugin : BaseUnityPlugin
+    {
+		public void Start()
+        {
+
+			GameObject Mirror = GameObject.Find("Level/lower level/mirror (1)");
+			Mirror.SetActive(true);
+			GameObject Camera = GameObject.Find("Level/lower level/mirror (1)/Camera");
+			Camera.GetComponent<Camera>().allowHDR = false;
+			Camera.GetComponent<Camera>().allowMSAA = false;
+		}
+	}
+}
